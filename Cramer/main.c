3136fdda0cec4792853bb622393326e72eaab1db
HabibAroua <habibha.aroua82@gmail.com>
2018-01-29 20:49:46 +0100

    give the size of matrix

--- Cramer/main.c
+++ Cramer/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void saisie(int *n);
+void remplir(int n , int M[15][15]);
+void afficher(int n , int M[15][15]);
+
 int main()
 {
     printf("Hello world!\n");
     return 0;
+}
+
+void saisie(int *n)
+{
+   int x;
+   do
+   {
+       printf("Donner la taille de matrice \n");
+       scanf("%d",&x);
+   }while(x<2);
+   *n=x;
+}
+
+void remplir(int n , int M[15][15])
+{
+}
+
+void afficher(int n , int M[15][15])
+{
 }
