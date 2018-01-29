262347240ed66c09b12d2ebf4a50b29b2a50c13c
HabibAroua <habibha.aroua82@gmail.com>
2018-01-29 20:57:16 +0100

    Shwing all element of matrix

--- Cramer/main.c
+++ Cramer/main.c
@@ -7,7 +7,11 @@ void afficher(int n , int M[15][15]);
 
 int main()
 {
-    printf("Hello world!\n");
+    int M[15][15];
+    int n;
+    saisie(&n);
+    remplir(n,M);
+    afficher(n,M);
     return 0;
 }
 
@@ -24,8 +28,26 @@ void saisie(int *n)
 
 void remplir(int n , int M[15][15])
 {
+     int i,j;
+     for(i=0 ; i<n ; i++)
+     {
+          for(j=0 ; j<n ; j++)
+          {
+               printf("Donner la ligne %d et la colonne %d \n",i+1,j+1);
+               scanf("%d",&M[i][j]);
+          }
+     }
 }
 
 void afficher(int n , int M[15][15])
 {
+     int i,j;
+     for(i=0 ; i<n; i++)
+     {
+          for(j=0 ; j<n ; j++)
+          {
+               printf("%d ",M[i][j]);
+          }
+          printf("\n");
+     }
 }
