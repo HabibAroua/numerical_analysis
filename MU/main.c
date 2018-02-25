a1287307099c0bdd2bab180a252a6e01ef7e1290
HabibAroua <habibha.aroua82@gmail.com>
2018-02-25 22:28:03 +0100

    Dev remplir method

--- MU/main.c
+++ MU/main.c
@@ -4,6 +4,7 @@
 void saisie(int *n);
 void remplir(int n , float A[100][100]);
 void LU(int n , float A[100][100]);
+void affichier(int n , float A[100][100]);
 
 int main()
 {
@@ -24,9 +25,22 @@ void saisie(int *n)
 
 void remplir(int n , float A[100][100])
 {
+    int i,j;
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+            printf("A[%d][%d]=\n",i+1,j+1);
+        }
+    }
+}
+
+void LU(int n , float A[100][100])
+{
 
 }
-void LU(int n , float A[100][100])
+
+void affichier(int n , float A[100][100])
 {
 
 }
