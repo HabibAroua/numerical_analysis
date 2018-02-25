4f8d57f45ca7a78c4ff9c6536a16d13321882510
HabibAroua <habibha.aroua82@gmail.com>
2018-02-25 22:22:38 +0100

    Edit method

---
+++ MU/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void saisie(int *n);
+void remplir(int n , float A[100][100]);
+void LU(int n , float A[100][100]);
+
+int main()
+{
+
+    return 0;
+}
+
+void saisie(int *n)
+{
+     int x;
+     do
+     {
+         printf("Donner la taille de la matrice \n");
+         scanf("%d")
+     }
+}
+
+void remplir(float n , int A[100][100])
+{
+
+}
+void LU(int n , float A[100][100])
+{
+
+}
